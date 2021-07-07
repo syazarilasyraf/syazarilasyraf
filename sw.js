0515ace9bd025bcac0b131545242af1970701d0e
Syazaril Asyraf <wanasyraf554@gmail.com>
2021-07-08 05:27:10 +0800

    installable

--- sw.js
+++ sw.js
@@ -11,6 +11,59 @@ self.addEventListener('fetch', event => {
     ${event.request.url}`);
 })
 
+// var CACHE_NAME = 'syazarilasyraf-cache-v2';
+// var urlsToCache = [
+//   'index.html',
+//   './',
+//   'assets/css/main.css',
+//   'icons/logo.ico'
+// ];
+
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+
+// self.addEventListener('activate', function(event) {
+//   console.log('Updating Service Worker...')
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.filter(function(cacheName) {
+//           // Return true if you want to remove this cache,
+//           // but remember that caches are shared across
+//           // the whole origin
+//           return true
+//         }).map(function(cacheName) {
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
+
 // self.addEventListener('install', function(event) {
 //     event.waitUntill(
 //         caches.open(sw-cache).then(function(cache) {
